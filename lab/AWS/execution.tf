f0d8b0fee09ceaaf4ed7e27e3ffa2296a7bb38c1
Sandeep Rawat <sandeep@opstree.com>
2018-01-26 12:17:39 +0530

    Now using publically available VPC module

--- lab/AWS/execution.tf
+++ lab/AWS/execution.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 module "jenkins_vpc" {
-  source   = "./vpc"
-  vpc_name = "${var.vpc_name}"
-  vpc_cidr = "${var.vpc_cidr}"
+  source = "github.com/opstree-terraform/vpc.git"
+  cidr   = "${var.vpc_cidr}"
+  name   = "${var.vpc_name}"
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id            = "${module.jenkins_vpc.vpc_id}"
+  vpc_id            = "${module.jenkins_vpc.id}"
   cidr_block        = "${var.subnet_cidr}"
   availability_zone = "${var.aws_region}a"
 
