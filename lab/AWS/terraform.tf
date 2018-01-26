94820e404205cd4c3bfb8267df5edfd53488672b
Sandeep Rawat <sandeep@opstree.com>
2018-01-26 11:25:03 +0530

    Externalized hardcoded values to variables

--- lab/AWS/terraform.tf
+++ lab/AWS/terraform.tf
@@ -1,7 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = "${var.aws_region}"
 }
 
 resource "aws_vpc" "jenkins-training-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "${var.vpc_cidr}"
 }
