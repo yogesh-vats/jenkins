e8ee5a6d83e64daeaa59a708fc24e799fad5ae79
Sandeep Rawat <sandeep@opstree.com>
2018-01-26 12:07:48 +0530

    Integrated local vpc module

--- lab/AWS/execution.tf
+++ lab/AWS/execution.tf
@@ -2,30 +2,14 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_vpc" "jenkins-training-vpc" {
-  cidr_block = "${var.vpc_cidr}"
-
-  tags {
-    Name = "${var.vpc_name}"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.jenkins-training-vpc.id}"
-
-  tags {
-    Name = "${var.vpc_name}-igw"
-  }
-}
-
-resource "aws_route" "public_internet_gateway_route" {
-  route_table_id         = "${aws_vpc.jenkins-training-vpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.igw.id}"
+module "jenkins_vpc" {
+  source   = "./vpc"
+  vpc_name = "${var.vpc_name}"
+  vpc_cidr = "${var.vpc_cidr}"
 }
 
 resource "aws_subnet" "subnet" {
-  vpc_id            = "${aws_vpc.jenkins-training-vpc.id}"
+  vpc_id            = "${module.jenkins_vpc.vpc_id}"
   cidr_block        = "${var.subnet_cidr}"
   availability_zone = "${var.aws_region}a"
 
