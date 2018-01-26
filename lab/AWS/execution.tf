66928aee00219b1300859aaff7ad51c34626addd
Sandeep Rawat <sandeep@opstree.com>
2018-01-26 11:39:23 +0530

    Added itnernet gateway route to main route table

--- lab/AWS/execution.tf
+++ lab/AWS/execution.tf
@@ -17,3 +17,9 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.vpc_name}-igw"
   }
 }
+
+resource "aws_route" "public_internet_gateway_route" {
+  route_table_id         = "${aws_vpc.jenkins-training-vpc.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.igw.id}"
+}
