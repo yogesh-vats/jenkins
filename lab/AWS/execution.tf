b0d8f22e75e6c0712a52ff35c16f4fbc6cb5be95
Sandeep Rawat <sandeep@opstree.com>
2018-01-26 11:35:37 +0530

    Added iternet gateway

--- lab/AWS/execution.tf
+++ lab/AWS/execution.tf
@@ -9,3 +9,11 @@ resource "aws_vpc" "jenkins-training-vpc" {
     Name = "${var.vpc_name}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.jenkins-training-vpc.id}"
+
+  tags {
+    Name = "${var.vpc_name}-igw"
+  }
+}
