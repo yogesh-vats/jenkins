c00400575e00e901103b5bee0409bd986dd99cc5
saurabhvaj <saurabhvaj>
2018-01-30 15:10:48 +0530

    updated path of job dsl

---
+++ lab/Jenkins/demo/jobdsl/ciseedjob.groovy
@@ -0,0 +1,27 @@
+job("HelloDSL") {
+ description("I\'ll say hello to person.")
+ logRotator(-1, 10)
+  parameters {	
+    stringParam('PROJECT', 'project-x', 'your project name')
+    stringParam('ENV', 'dev', 'your env name')
+	stringParam('REPOSITORY', 'https://github.com/OpsTree/ContinuousIntegration.git', 'your repo of code')
+	stringParam('BRANCH', 'master', 'your branch')
+	stringParam('POMRELATIVEPATH', 'Spring3HibernateApp', 'your pom\'s relative path')
+	stringParam('MVNGOALS', 'compile', 'EX: compile pmd:pmd checkstyle:checkstyle -DskipTests cobertura:cobertura -Dcobertura.report.format=xml')
+	stringParam('MAVENAME', 'default', 'This is the name/identifire of your maven.')
+	choiceParam('TEMPLATE', ['codestability', 'codequality', 'codecoverage'], 'your Job template')
+    }
+   scm {
+        git {
+      remote {
+        url("https://github.com/ot-training/jenkins.git")
+      }
+      branch("*/master")
+     }
+    } 
+   steps {
+    dsl {
+      external('lab/Jenkins/demo/jobdsl/ci/*.groovy')
+  }
+ }
+}
